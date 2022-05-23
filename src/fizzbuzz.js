b71f846bd96cb5a747f6695aa5499ab992d771c9
Lorel <lorel.oboue@gmail.com>
2022-05-23 01:15:19 +0100

    Fizz buzz challenge started

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,8 @@
 // TODO: Write your function in this file
 
+function fizzBuzz(lower, upper) {
+
+}
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
