24a83ae7de182f1f9a91e9b9123a5d7dd608b647
Lorel <lorel.oboue@gmail.com>
2022-05-23 11:15:40 +0100

    fizzbuzz challenge completed with extension

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,44 @@
 // TODO: Write your function in this file
 
-function fizzBuzz(lower, upper) {
+const fizzBuzzValues = {
+    fistNumber: {
+        value: 3,
+        string: 'Fizz'
+    },
 
+    secondNumber: {
+        value: 5,
+        string: 'Buzz'
+    },
+
+    bothNumbers: {
+        string: 'FizzBuzz'
+    }
 }
+
+function fizzBuzz(lower, upper) {
+    let fizzBuzzList = []
+
+    for(let i = lower; i <= upper; i++) {
+        if(i % fizzBuzzValues.fistNumber.value === 0 && i % fizzBuzzValues.secondNumber.value !== 0) {
+           fizzBuzzList.push(fizzBuzzValues.fistNumber.string) 
+        } else if 
+            (i % fizzBuzzValues.secondNumber.value === 0 && i % fizzBuzzValues.fistNumber.value !== 0) {
+            fizzBuzzList.push(fizzBuzzValues.secondNumber.string)
+            }
+         else if 
+         (i % fizzBuzzValues.fistNumber.value === 0 && i % fizzBuzzValues.secondNumber.value === 0) { fizzBuzzList.push(fizzBuzzValues.bothNumbers.string)     
+        } 
+
+         else {
+        fizzBuzzList.push(i)
+        }
+}
+    return fizzBuzzList;
+}
+
+
+// had to make sure i = lower and not 0
 
 // TODO: Change undefined below to the name of your function
 module.exports = fizzBuzz
